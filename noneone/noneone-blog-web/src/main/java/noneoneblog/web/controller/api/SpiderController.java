0a913ee34dea6cbfdd61dfd77291ce4f2d86cc5a
379106481@qq.com <lifeifei168>
2017-11-21 17:36:12 +0800

    爬虫推送文章

--- noneone/noneone-blog-web/src/main/java/noneoneblog/web/controller/api/SpiderController.java
+++ noneone/noneone-blog-web/src/main/java/noneoneblog/web/controller/api/SpiderController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
 
@@ -33,6 +34,7 @@ public class SpiderController extends BaseController{
 	 * @return
 	 */
 	@RequestMapping(value = "/spiderSubmit", method = RequestMethod.POST)
+	 @ResponseBody
 	public String spiderPost(String post, HttpServletRequest request) {
 		String text = null;
 		try {
