4bf5375822551523aca482219928b1e5325f0a27
379106481@qq.com <lifeifei168>
2017-11-30 11:23:41 +0800

    时间算法

--- noneone/noneone-blog-base/src/main/java/noneoneblog/base/utils/RelativeDateFormat.java
+++ noneone/noneone-blog-base/src/main/java/noneoneblog/base/utils/RelativeDateFormat.java
@@ -66,8 +66,36 @@ public class RelativeDateFormat {
 	 * @return
 	 */
 	private static int getDaysToday(Date date) {
-		int days = (int) ((new Date().getTime() - date.getTime()) / (1000 * 3600 * 24));
-		return days;
+		// int days = (int) ((new Date().getTime() - date.getTime()) / (1000 *
+		// 3600 * 24));
+		Calendar cal1 = Calendar.getInstance();
+		cal1.setTime(date);
+
+		Calendar cal2 = Calendar.getInstance();
+		cal2.setTime(new Date());
+		int day1 = cal1.get(Calendar.DAY_OF_YEAR);
+		int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+
+		int year1 = cal1.get(Calendar.YEAR);
+		int year2 = cal2.get(Calendar.YEAR);
+		if (year1 != year2) // 同一年
+		{
+			int timeDistance = 0;
+			for (int i = year1; i < year2; i++) {
+				if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) // 闰年
+				{
+					timeDistance += 366;
+				} else // 不是闰年
+				{
+					timeDistance += 365;
+				}
+			}
+
+			return timeDistance + (day2 - day1);
+		} else // 不同年
+		{
+			return day2 - day1;
+		}
 	}
 
 	private static long toSeconds(long date) {
